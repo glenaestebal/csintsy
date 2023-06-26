84486c9241a479d4d7422580e5e023a2b52593de
glena estebal <eidrene_glena_estebal@dlsu.edu.ph>
2023-06-26 15:05:41 +0800

    changed reference code and updated to do list

--- dfs.py
+++ dfs.py
@@ -1,5 +1,5 @@
 
-# REFERENCE: https://favtutor.com/blogs/depth-first-search-python
+# REFERENCE: https://www.scaler.com/topics/dfs-python/
 
 # code with start node to goal node
 # https://stackabuse.com/courses/graphs-in-python-theory-and-implementation/lessons/depth-first-search-dfs-algorithm/ 
@@ -8,29 +8,34 @@
 TO DO
  [] get input from user
  [] start node and goal node (kase we need to find the optimal find to start node to goal node)
-
+ [] showcase the existing graph
+ [] edit the existing nodes
+ [] terminate the code to exit
 """
 
 
-# Using a Python dictionary to act as an adjacency list
-graph = {
-  '5' : ['3','7'],
-  '3' : ['2', '4'],
-  '7' : ['8'],
-  '2' : [],
-  '4' : ['8'],
-  '8' : []
-}
+def dfs(node, graph, visited, component):
+    component.append(node)  # Store answer
+    visited[node] = True  # Mark visited
 
-visited = set() # Set to keep track of visited nodes of graph.
+    # Traverse to each adjacent node of a node
+    for child in graph[node]:
+        if not visited[child]:  # Check whether the node is visited or not
+            dfs(child, graph, visited, component)  # Call the dfs recursively
 
-def dfs(visited, graph, node):  #function for dfs 
-    if node not in visited:
-        print (node)
-        visited.add(node)
-        for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
 
-# Driver Code
-print("Following is the Depth-First Search")
-dfs(visited, graph, '5')
+if __name__ == "__main__":
+
+    # Graph of nodes
+    graph = {
+        0: [2],
+        1: [2, 3],
+        2: [0, 1, 4],
+        3: [1, 4],
+        4: [2, 3]
+    }
+    node = 0  # Starting node
+    visited = [False]*len(graph)  # Make all nodes to False initially
+    component = []
+    dfs(node, graph, visited, component)  # Traverse to each node of a graph
+    print(f"Following is the Depth-first search: {component}")  # Print the answer
